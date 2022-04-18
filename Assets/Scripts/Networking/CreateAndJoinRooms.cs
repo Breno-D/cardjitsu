6b78141d636da5ca4a77a420be638e7c6645af66
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2022-04-18 02:13:14 -0300

    [ADD] Basic Network

---
+++ Assets/Scripts/Networking/CreateAndJoinRooms.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createInput, joinInput;
+    
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
